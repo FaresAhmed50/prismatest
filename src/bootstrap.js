8f32e7e41936398cf23dc340b9e707fbcf91fb5e
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2026-01-18 01:35:13 +0200

    Config node boilder plate with prisma postgress integration

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,16 +1,8 @@
-import prisma from "../prisma/prisma.client.js";
 
 
 const bootstrap = async (app, express) => {
 
     app.use(express.json());
-
-
-    const x = await prisma.test.findMany();
-    console.log(x)
-
-
-
 }
 
 
